52b7983c40f14b56dee4abb3cabe6013b12c555b
Thanmayee B N <thanmayeebn@gmail.com>
2023-02-03 23:48:56 +0530

    Updates

---
+++ precache-manifest.2f1c4b2462d7b8d398d8f2fa193a67a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3afd1a6b10644271ee51",
+    "url": "/ThanmayeeBN/Portfolio/static/css/main.d9ca012e.chunk.css"
+  },
+  {
+    "revision": "3afd1a6b10644271ee51",
+    "url": "/ThanmayeeBN/Portfolio/static/js/main.857095a6.chunk.js"
+  },
+  {
+    "revision": "08fde9f8138cab45ca0e",
+    "url": "/ThanmayeeBN/Portfolio/static/js/runtime~main.c7968179.js"
+  },
+  {
+    "revision": "640fcf205b489e365379",
+    "url": "/ThanmayeeBN/Portfolio/static/js/2.a76f7fa7.chunk.js"
+  },
+  {
+    "revision": "ee196ffb84bcc878878327f782711105",
+    "url": "/ThanmayeeBN/Portfolio/static/media/ProfilePicture.ee196ffb.jpg"
+  },
+  {
+    "revision": "1ba7c23b5070af899c89467fec450fea",
+    "url": "/ThanmayeeBN/Portfolio/static/media/Thanmayee.1ba7c23b.pdf"
+  },
+  {
+    "revision": "c5fcc43c4420c543aaa6f97c2a4ce3a9",
+    "url": "/ThanmayeeBN/Portfolio/index.html"
+  }
+];
